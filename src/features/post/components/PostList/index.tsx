31e50518f2bccf234cb3de58ce85d5d4cf6be65c
bayudha2 <bayudha.bd@gmail.com>
2022-08-26 18:09:36 +0700

    chore: add select user at status page

--- src/features/post/components/PostList/index.tsx
+++ src/features/post/components/PostList/index.tsx
@@ -5,12 +5,13 @@ import { useGetPostsQuery } from '../../services/postExtend';
 import CardPost from 'src/components/cards/CardPost';
 
 const PostList = ({ id }: { id: string | number | unknown }) => {
-  const { isLoading, isSuccess, isError, data } = useGetPostsQuery(id);
+  const { isLoading, isSuccess, isError, isFetching, data } = useGetPostsQuery(id);
   const skeleton = [...Array(10)];
 
   return (
     <div className="flex flex-col justify-center gap-4 overflow-hidden">
-      {isLoading && skeleton.map((_, i) => <Skeleton key={i} height={100} width={300} />)}
+      {isLoading ||
+        (isFetching && skeleton.map((_, i) => <Skeleton key={i} height={100} width={300} />))}
       {isSuccess && data.map((data) => <CardPost {...data} key={data.id} />)}
       {isError && <h1 className="font-bold text-6xl">Oops something went wrong…</h1>}
     </div>
--- src/pages/Status/index.tsx
+++ src/pages/Status/index.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 import Modal from 'src/components/Modal';
 import { PostList } from 'src/features/post';
-import { useGetUserQuery } from 'src/features/user';
+import { useGetUserQuery, useGetUsersQuery } from 'src/features/user';
 import { toggleModal } from 'src/helper/helperSlice';
 import { useAppDispatch, useAppSelector } from 'src/hooks/reduxHooks';
 
 const Status = () => {
   const location = useLocation();
   const state = location.state as { id: string };
+
+  const userRef = useRef<HTMLSelectElement>();
+  const [userId, setUserId] = useState<number | string>(state?.id ? state.id : 1);
+
+  useEffect(() => {
+    setUserId(state?.id ? state.id : 1);
+    console.log('userId', userId);
+  }, []);
+
   const dispatch = useAppDispatch();
   const isModalOpen = useAppSelector((state) => state.helper.openModal);
 
-  const { isSuccess, data } = useGetUserQuery(state?.id ? state.id : 1);
+  const { data: usersData, isLoading, isError } = useGetUsersQuery();
+  const { isSuccess, data } = useGetUserQuery(userId);
 
   function handleOpenModal(): void {
     dispatch(toggleModal({ data: {}, type: 'createPost' }));
   }
 
+  function handleUserChange() {
+    setUserId(userRef.current.value);
+  }
+
   return (
     <>
       <section className="mt-10">
+        <div>
+          <label htmlFor="user" className="relative">
+            <select
+              ref={userRef}
+              onChange={handleUserChange}
+              name="user"
+              id="user"
+              defaultValue={userId}
+              className="select-user-input">
+              {isLoading || (isError && <option value="1">User</option>)}
+              {usersData &&
+                usersData.map((item) => {
+                  return (
+                    <option key={item.id} value={item.id}>
+                      {item.name}
+                    </option>
+                  );
+                })}
+            </select>
+            <ChevronDownIcon className="h-4 w-4 absolute pointer-events-none z-10 top-0 right-5 text-gray-700" />
+          </label>
+        </div>
         <div className="mt-10">
           <small className="text-gray-600">List of Post</small>
           <h1 className="mb-4 mt-3 text-lg ">
@@ -34,7 +71,7 @@ const Status = () => {
           className="py-1 text-sm text-cyan-50 mb-4 px-2 rounded-md bg-main-30">
           Create post
         </button>
-        <PostList id={state?.id ? state.id : 1} />
+        <PostList id={userId} />
       </section>
       {isModalOpen && <Modal />}
     </>
