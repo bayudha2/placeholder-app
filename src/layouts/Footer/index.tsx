bbdff93f4a4ec719deaa28ca96911834b4ee3299
bayudha2 <bayudha.bd@gmail.com>
2022-08-27 12:51:54 +0700

    chore: add footer component + remove unused file

---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="w-full py-8">
+      <p className="text-center text-sm text-gray-600">Powered by JSONPLACEHOLDER</p>
+    </div>
+  );
+};
+
+export default Footer;
